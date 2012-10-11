fa1d17ba08370efd860a720f71359914f17b3ae6
KAZUMA Ukyo <yaakaito@gmail.com>
2012-10-11 15:48:26 +0900

    sample layout script

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function(){
+  
+  var base = { w : 1160 , h : 752 },
+  header = document.querySelector("body > header"),
+  footer = document.querySelector("body > footer"),
+  svg = document.querySelector("body > svg"),
+  layout = function() {
+    var width = window.innerWidth,
+      height = window.innerHeight,
+      marginHeight = height / 10,
+      svgHeight = marginHeight * 8;;
+    header.style.height = marginHeight + "px";
+    footer.style.height = marginHeight + "px"; 
+    svg.style.top = marginHeight + "px";
+    svg.setAttribute("height", svgHeight);
+    svg.setAttribute("width", svgHeight * (1160 / 752));
+  
+  window.onresize = layout;
+
+  layout();
+})();
